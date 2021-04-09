aa7de51becbb14a2fddb742aaccd48402d41955b
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2021-04-09 13:45:54 +1200

    works

--- src/run.ts
+++ src/run.ts
@@ -2,4 +2,13 @@ import { Solver } from "./solver";
 import board from './data.json';
 
 const solver = new Solver(board);
-solver.solve()
+console.log('init')
+console.log(solver.board)
+const result = solver.solve();
+if(result) {
+  console.log('solved in ', solver.turns)
+  console.log(solver.board)
+} else {
+  console.log('unsolved in ', solver.turns)
+  console.log(solver.board)
+}
